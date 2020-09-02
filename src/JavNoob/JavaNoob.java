2d1212206d76a303d2cc1ba4c286c278e31a1774
Saint <roman11rr@mail.ru>
2020-09-02 10:38:14 +0300

    New

---
+++ src/JavNoob/JavaNoob.java
@@ -0,0 +1,11 @@
+package JavNoob;
+import java.lang.System;
+import java.util.*;
+
+public class JavaNoob
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Java");
+    }
+}
